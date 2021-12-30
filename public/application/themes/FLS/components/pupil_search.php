690db802923bc9e3da6b3a4feced7f452683dbce
jon chandler <jonc@mcsaatchi.com>
2021-12-30 10:11:00 +0000

    form

--- public/application/themes/FLS/components/pupil_search.php
+++ public/application/themes/FLS/components/pupil_search.php
@@ -1,9 +1,9 @@
 <?php 
 	defined('C5_EXECUTE') or die('Access Denied.'); 
 ?>
-<div class="pupil-search <?php echo $class; ?>">
-	<form method="get" action="<?php echo View::url('/pupil_list'); ?>" role="search">
+<form method="get" action="<?php echo View::url('/pupil_list'); ?>" role="search">
+	<div class="pupil-search <?php echo $class; ?>">
 		<input name="query" class="search-field" type="text" value="<?php echo htmlentities($query, ENT_COMPAT, APP_CHARSET)?>" maxlength="150" placeholder="<?php echo t('pupil name'); ?>" />
 		<button class="data-call-btn" title="<?php echo t('Search'); ?>"></button>
-	</form>
-</div>
+	</div>
+</form>
